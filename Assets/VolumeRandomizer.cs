45efa7c52ee91debabf001466ef1cf4e9e9f5583
Kyle <arow411@gmail.com>
2019-09-04 12:07:10 -0400

    Added audio

---
+++ Assets/VolumeRandomizer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class VolumeRandomizer : MonoBehaviour
+{
+    private Vector2 _dir;
+    private AudioSource audsrc;
+
+    void Start()
+    {
+        audsrc = GetComponent<AudioSource>();
+
+        var a = Random.value * Mathf.PI * 2f;
+        _dir = new Vector2(Mathf.Cos(a), Mathf.Sin(a));
+    }
+
+    void Update()
+    {
+        var v = _dir * Time.time / 10f;
+        float turb = Mathf.PerlinNoise(v.x, v.y);
+
+        audsrc.volume = (turb - 0.3f) / 1.5f;
+    }
+
+}
